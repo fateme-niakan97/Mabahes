1a8a3bc10d8ae9f76e3ffd90d9ffa186aef3b026
SarvinSaravi <sarvin2000@gmail.com>
2020-05-13 14:42:40 +0430

    Sarvin changes

--- P1/Image_Enhancement.py
+++ P1/Image_Enhancement.py
@@ -7,10 +7,10 @@ print(filename)
 list_of_images = []
 
 for item in filename:
-    img = io.imread(item)
+    img = io.imread('pictures\\' + item)
     list_of_images.append(img)
 
-print(list_of_images)
+print(len(list_of_images))
 
 # list_pics = io.imread(filename)
 
