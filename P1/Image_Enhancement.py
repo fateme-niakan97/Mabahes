0c3d39ab12993bf54feec1710bb94a71911b14b7
SarvinSaravi <sarvin2000@gmail.com>
2020-05-13 14:26:18 +0430

    Sarvin changes

--- P1/Image_Enhancement.py
+++ P1/Image_Enhancement.py
@@ -1,7 +1,10 @@
 import os
 from skimage import io
+import matplotlib.pyplot as plt
 
-filename = os.path.join('P1/pictures/1.png')
+filename = os.path.join('pictures/1.png')
+print('here1')
 moon = io.imread(filename)
 
-
+print(moon.shape)
+plt.imshow(moon)
